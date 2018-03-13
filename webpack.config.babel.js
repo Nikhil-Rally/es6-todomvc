e109e65b138065d6e694f760f610f6be03746e65
George Spake <gpspake@gmail.com>
2018-03-12 22:20:08 -0500

    add source maps and env flag

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,14 @@
 import path from 'path';
 
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isDev = nodeEnv === 'development';
+
 module.exports = {
   entry: './src/bootstrap.js',
   output: {
     path: path.resolve('dist'),
     filename: 'bundle.js',
     publicPath: '/dist/'
-  }
+  },
+  devtool: isDev ? 'eval' : 'source-map'
 };
