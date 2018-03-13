79f17ecba6cd76255bcefb3c7b69056f955e5d16
George Spake <gpspake@gmail.com>
2018-03-12 21:56:04 -0500

    add path configuration

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,10 @@
+import path from 'path';
+
 module.exports = {
   entry: './src/bootstrap.js',
   output: {
-    filename: 'bundle.js'
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
   }
 };
