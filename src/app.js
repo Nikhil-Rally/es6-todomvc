9bfaa8ca3dc398a41228e3e26978a2e6da8bbec5
George Spake <gpspake@gmail.com>
2018-03-26 10:23:24 -0500

    add click handler to toggle graph button

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 import 'todomvc-app-css/index.css'
 import './app.css'
+import {$on} from './helpers'
 import {updateTodo} from './todo'
 
 export function onLoad() {
   updateTodo()
+  const toggleGraphButton = document.querySelector('.toggle-graph')
+  $on(
+    toggleGraphButton,
+    'click',
+    () => {
+      console.log('click')
+    },
+  )
 }
